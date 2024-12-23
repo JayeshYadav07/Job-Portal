3f78ebee176431a82e08bc4b519d447690fc93ed
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-12-23 14:10:31 +0530

    Dockerize the application

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect(
+			process.env.MONGO_URI || "mongodb://localhost:27017/job-portal"
+		);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Connection to MongoDB failed. Error: ${error.message}`);
